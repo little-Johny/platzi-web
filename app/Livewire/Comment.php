49e18e678da7dfc2f802d050cdabcc8076b8a3c7
little-Johny <molano.johny721@gmail.com>
2025-10-14 00:03:26 -0500

    feat(forum): add comment submission functionality
    Implement the ability to add comments through a form in the Livewire component,
    including validation, creation, and UI updates for the comment system.

--- app/Livewire/Comment.php
+++ app/Livewire/Comment.php
@@ -9,10 +9,29 @@ class Comment extends Component
 {
     public Model $commentable;
     public bool $showForm = false;
+    public string $content = '';
 
     public function toggle()
     {
         $this->showForm = !$this->showForm;
+    }
+
+    public function add()
+    {
+        $this->validate([
+            'content' => 'required|string|max:255',
+        ]);
+
+        $this->commentable->comments()->create([
+            'content' => $this->content,
+            'user_id' => 1, // Reemplazar por el id del usuario de la sesion
+        ]);
+
+        // Reiniciar variables
+        /* $this->content = '';
+        $this->showForm = false; */
+
+        $this->reset('content','showForm');
     }
 
     public function render()
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,6 +10,11 @@ class Comment extends Model
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'content',
+        'user_id'
+    ];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
